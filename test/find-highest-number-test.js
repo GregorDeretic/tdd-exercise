b8c7e45ffaa47e11ffc3436c7f1bee3f1b4cc0b1
Gregor Deretic <gregor.deretic@sky.uk>
2021-01-30 11:09:13 +0000

    third push

--- test/find-highest-number-test.js
+++ test/find-highest-number-test.js
@@ -7,30 +7,38 @@ class HighestNumber
 {
     findHighestNumber( arrayToCheck )
     {
+        if (arrayToCheck.length > 0) {
+            
+            var b = arrayToCheck[0];
+            var i;
+            var j ;
 
-        var b;
-        var i;
-        var j ;
-
-        for (i = 0; i < arrayToCheck.length;) {
-            for (j = 1; j < arrayToCheck.length -1;) {
-    
-                console.log('for i :', i);
-                console.log('for j :', j);
-                if (arrayToCheck[i] > arrayToCheck[j]) {
-                    b = arrayToCheck[i];
-                    j++;
-                    console.log('first i :', i);
-                    console.log('first j :', j);
-                } else {
-                    b = arrayToCheck[j];
-                    i = j;
-                    j++;
-                    console.log('second i :', i);
-                    console.log('second j :', j);
+            for (i = 0; i < arrayToCheck.length;)
+            {
+                for (j = 1; j < arrayToCheck.length;)
+                {
+                    console.log('for i :', i);
+                    console.log('for j :', j);
+                    if (arrayToCheck[i] > arrayToCheck[j])
+                    {
+                        b = arrayToCheck[i];
+                        j++;
+                        console.log('first i :', i);
+                        console.log('first j :', j);
+                    } else
+                    {
+                        b = arrayToCheck[j];
+                        i = j;
+                        j++;
+                        console.log('second i :', i);
+                        console.log('second j :', j);
+                    }
                 }
+                return b;
             }
-        return b;
+
+            
+        
         }
         
     }  
@@ -52,6 +60,21 @@ describe('find the highest number', function() {
         assert.equal(actualResult, expectedResult);
         expect(actualResult).to.eql(expectedResult);
 
+    });
+
+    it('return highest item in array with 2 elements', function() {
+        // arrange...
+        var cut = new HighestNumber();
+        var expectedResult = 13;
+        var inputArray = [13, 7];
+
+        // act...
+        var actualResult = cut.findHighestNumber(inputArray);
+        console.log(actualResult)
+
+        // assert...
+        assert.equal(actualResult, expectedResult);
+        expect(actualResult).to.eql(expectedResult);
 
     });
 
@@ -69,6 +92,22 @@ describe('find the highest number', function() {
         assert.equal(actualResult, expectedResult);
         expect(actualResult).to.eql(expectedResult);
 
+
+    });
+
+    it('If several numbers are equal and highest, only one should be returned', function() {
+        // arrange...
+        var cut = new HighestNumber();
+        var expectedResult = 11
+        var inputArray = [4, 5, -8, 3, 11, -21, 6, 11];
+
+        // act...
+        var actualResult = cut.findHighestNumber(inputArray);
+        console.log(actualResult)
+
+        // assert...
+        assert.equal(actualResult, expectedResult);
+        expect(actualResult).to.eql(expectedResult);
 
     });
 
